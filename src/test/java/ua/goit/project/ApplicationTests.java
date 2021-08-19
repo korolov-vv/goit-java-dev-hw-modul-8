a3f41f492b21d622ac82271d751669e830aee2a0
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-08-20 00:47:27 +0200

    Update

--- src/test/java/ua/goit/project/ApplicationTests.java
+++ src/test/java/ua/goit/project/ApplicationTests.java
@@ -1,13 +1,12 @@
 package ua.goit.project;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
+/*	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
